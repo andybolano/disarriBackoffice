689ce38c29c0c2a86f55a0869a41500efac0a7b8
User <User@DESKTOP-22OLGMQ>
2021-09-23 18:32:51 -0500

    change url

--- src/app/services/servers.service.ts
+++ src/app/services/servers.service.ts
@@ -5,7 +5,7 @@ import { Headers, RequestOptions } from '@angular/http';
 export class ServersService {
     constructor() { }
     //serverName: string = "http://localhost/deirisarri/api/public/index.php/api"
-    serverName: string = "https://disarri.lauchoit.com/api"
+    serverName: string = "https://deirisarri.co/api/public/api"
     storageGrupo(codigo_grupo) {
         localStorage.setItem('session_group', codigo_grupo);
     }
